a4e3f85130716a99b94b10336e95d41d11372323
Andrey Lazarko <Lazark0x@users.noreply.github.com>
2025-09-18 15:04:55 +0200

    visibility hidden for wasm

--- crypto/fipsmodule/bn/internal.h
+++ crypto/fipsmodule/bn/internal.h
@@ -119,9 +119,9 @@ static inline void bn_mul_mont_small(
     bn_mul_mont_nohw(rp, ap, bp, np, n0, num);
 }
 #else
-void bn_mul_mont(BN_ULONG *rp, const BN_ULONG *ap, const BN_ULONG *bp,
+RING_WASM_HIDDEN void bn_mul_mont(BN_ULONG *rp, const BN_ULONG *ap, const BN_ULONG *bp,
                  const BN_ULONG *np, const BN_ULONG *n0, size_t num);
-static inline void bn_mul_mont_small(
+RING_WASM_HIDDEN static inline void bn_mul_mont_small(
     BN_ULONG *rp, const BN_ULONG *ap, const BN_ULONG *bp,
     const BN_ULONG *np, const BN_ULONG *n0, size_t num) {
     bn_mul_mont(rp, ap, bp, np, n0, num);
