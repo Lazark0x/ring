2b73adead984fb2efd12c4d4c1937cb75b47812d
Andrey Lazarko <Lazark0x@users.noreply.github.com>
2025-09-19 15:07:43 +0200

    Update montgomery.rs

--- src/arithmetic/montgomery.rs
+++ src/arithmetic/montgomery.rs
@@ -209,19 +209,6 @@ cfg_if! {
             }
         }
 
-        #[cfg(target_arch = "wasm32")]
-        #[no_mangle]
-        unsafe extern "C" fn __ring_bn_mul_mont_impl(
-            r: *mut Limb,
-            a: *const Limb,
-            b: *const Limb,
-            n: *const Limb,
-            n0: &N0,
-            num_limbs: c::NonZero_size_t,
-        ) {
-            unsafe { bn_mul_mont_fallback(r, a, b, n, n0, num_limbs) }
-        }
-
         #[cfg_attr(target_arch = "x86", cold)]
         #[cfg_attr(target_arch = "x86", inline(never))]
         unsafe extern "C" fn bn_mul_mont_fallback(
